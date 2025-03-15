b3098cdd770697495f39c34e8f9cd5bf997f356d
Siriphonn <siriphon.su@ku.th>
2025-03-16 00:27:37 +0700

    แก้ไขหน้า login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,13 +4,12 @@ import { useNavigate } from "react-router-dom";
 function Login({ setUser }) {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState(""); // เพิ่มการจัดการกับรหัสผ่าน
   const navigate = useNavigate();
 
   const handleLogin = () => {
     // ตรวจสอบข้อมูลการเข้าสู่ระบบ
-    if (username === "admin" && password === "admin123") {
-      setUser({ name: "Admin", email: "admin@example.com", isAdmin: true }); // สำหรับ Admin
+    if (username === "admin" && email === "admin@bunny.com") {
+      setUser({ name: "Admin", email: "admin@bunny.com", isAdmin: true }); // สำหรับ Admin
       navigate("/bookings");
     } else if (username && email) {
       setUser({ name: username, email: email, isAdmin: false }); // สำหรับ User ทั่วไป
@@ -40,12 +39,6 @@ function Login({ setUser }) {
         />
       </div>
       <div>
-        <label>Password:</label>
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
       </div>
       <button onClick={handleLogin}>Login</button>
     </div>
